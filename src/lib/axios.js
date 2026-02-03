b8c094d33b045d86bd808186c1c71bbe372412c9
Subhajit Maity <maitysubhajit872@gmail.com>
2026-02-03 08:00:47 +0530

    frontend ready

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,13 @@
+// import axios from "axios";
+
+// export const axiosInstance = 
+//     axios.create({
+//        baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+//        withCredentials: true,
+//     })
 import axios from "axios";
 
-export const axiosInstance = 
-    axios.create({
-       baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
-       withCredentials: true,
-    })
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+});
