91d3823baccd51a5b36ced06c7385e7027b7495d
Subhajit Maity <maitysubhajit872@gmail.com>
2026-02-03 11:34:02 +0530

    change url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -5,9 +5,16 @@
 //        baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
 //        withCredentials: true,
 //     })
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL,
+//   withCredentials: true,
+// });
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://chatify-backend-8enc.onrender.com",
   withCredentials: true,
 });
+
